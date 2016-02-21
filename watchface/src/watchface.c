e3261292f4c366cb7adf1a588f7c739a2e7b5dc4
psanchezp <partricios94@gmail.com>
2016-02-20 19:11:42 -0600

    Timer works

--- watchface/src/watchface.c
+++ watchface/src/watchface.c
@@ -1,12 +1,16 @@
 /*
  * main.c
- * Sets up the Window, ClickConfigProvider and ClickHandlers.
+ * Sets up the timer and resets it
  */
 
 #include <pebble.h>
 
 static Window *s_main_window;
 static TextLayer *s_output_layer;
+
+//Layer de tiempo
+static TextLayer *s_uptime_layer;
+static int s_uptime = 0;
 
 /******* Methods ********/
 void vibrations (int times){
@@ -21,14 +25,37 @@ void vibrations (int times){
   }
 }
 
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  // Use a long-lived buffer
+  static char s_uptime_buffer[32];
+
+  // Get time since launch
+  int seconds = s_uptime % 60;
+  int minutes = (s_uptime % 3600) / 60;
+  int hours = s_uptime / 3600;
+
+  // Update the TextLayer
+  snprintf(s_uptime_buffer, sizeof(s_uptime_buffer), "Uptime: %dh %dm %ds", hours, minutes, seconds);
+  text_layer_set_text(s_uptime_layer, s_uptime_buffer);
+
+  // Increment s_uptime
+  s_uptime++;
+
+  //Uptime Vibration
+  if (seconds%5 == 0){
+    vibrations(3);
+  }
+}
+
 /******* Controllers **********/
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(s_output_layer, "Up pressed!");
 }
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_output_layer, "Select pressed!");
-  vibrations(3);
+  text_layer_set_text(s_output_layer, "Timer Begins");
+  //vibrations(3);
+  s_uptime = 0;
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -52,11 +79,21 @@ static void main_window_load(Window *window) {
   text_layer_set_text(s_output_layer, "No button pressed yet.");
   text_layer_set_overflow_mode(s_output_layer, GTextOverflowModeWordWrap);
   layer_add_child(window_layer, text_layer_get_layer(s_output_layer));
+
+  // Create output TextLayer
+  s_uptime_layer = text_layer_create(GRect(0, 100, window_bounds.size.w, window_bounds.size.h));
+  text_layer_set_font(s_output_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_text_alignment(s_uptime_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_uptime_layer, "Uptime: 0h 0m 0s");
+  layer_add_child(window_layer, text_layer_get_layer(s_uptime_layer));
 }
 
 static void main_window_unload(Window *window) {
   // Destroy output TextLayer
   text_layer_destroy(s_output_layer);
+
+  // Destroy output Uptime
+  text_layer_destroy(s_uptime_layer);
 }
 
 static void init() {
@@ -68,6 +105,9 @@ static void init() {
   });
   window_set_click_config_provider(s_main_window, click_config_provider);
   window_stack_push(s_main_window, true);
+
+  // Subscribe to TickTimerService
+  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
 }
 
 static void deinit() {
