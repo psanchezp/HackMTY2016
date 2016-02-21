b959cb7d502b99bf4d649472a1dfb07644ea7550
psanchezp <partricios94@gmail.com>
2016-02-20 18:26:36 -0600

    Branch Ready

--- watchface/src/watchface.c
+++ watchface/src/watchface.c
@@ -1,25 +1,33 @@
 #include <pebble.h>
-
-Window *window; 
+Window *window;
 TextLayer *text_layer;
 
 void init() {
   // Create the Window
   window = window_create();
-
   // Push to the stack, animated
   window_stack_push(window, true);
-
   // Create the TextLayer, for display at (0, 0),
   // and with a size of 144 x 40
   text_layer = text_layer_create(GRect(0, 0, 144, 40));
-
   // Set the text that the TextLayer will display
   text_layer_set_text(text_layer, "PATETOOO! ");
-
   // Add as child layer to be included in rendering
-  layer_add_child(window_get_root_layer(window), 
-    text_layer_get_layer(text_layer));
+  layer_add_child(window_get_root_layer(window),
+  text_layer_get_layer(text_layer));
+  window_set_background_color(window, GColorBlack);
+  //Vibrations
+  //Otro
+  int times = 3;
+  static const uint32_t const segments[] = { 200, 100, 400 };
+  if (times > 0){
+  VibePattern pat = {
+  .durations = segments,
+  .num_segments = ARRAY_LENGTH(segments),
+  };
+  vibes_enqueue_custom_pattern(pat);
+  times --;
+  }
 }
 
 void deinit() {
@@ -30,15 +38,12 @@ void deinit() {
 }
 
 int main(void) {
-    // Initialize the app
+  // Initialize the app
   init();
-
   // Wait for app events
   app_event_loop();
-
   // Deinitialize the app
   deinit();
-
   // App finished without error
   return 0;
 }
